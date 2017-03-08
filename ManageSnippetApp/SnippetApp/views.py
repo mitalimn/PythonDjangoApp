946db45b0045c7a30cfa035b9220c52d07aa9189
mitalimn <mitalinaik@yahoo.com>
2017-03-08 15:37:57 -0600

    template is working

--- ManageSnippetApp/SnippetApp/views.py
+++ ManageSnippetApp/SnippetApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+ # SnippetApp/views.py
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
