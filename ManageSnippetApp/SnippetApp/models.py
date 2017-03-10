9c8b3264991f1550732235a80775f3ed5813a416
mitalimn <mitalinaik@yahoo.com>
2017-03-10 13:51:26 -0600

    post method saves data in django database

--- ManageSnippetApp/SnippetApp/models.py
+++ ManageSnippetApp/SnippetApp/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Snippet(models.Model):
-    title = models.CharField(max_length=30)
-    language = models.CharField(max_length=30)
-    code_snippet = models.CharField(max_length=50)
-    description = models.CharField(max_length=50);
+    title = models.CharField(max_length=30, blank=False, null=False)
+    language = models.CharField(max_length=30, blank=False, null=False)
+    code_snippet = models.CharField(max_length=50, blank=False, null=False)
+    description = models.CharField(max_length=50, blank=False, null=False)
+
--- ManageSnippetApp/SnippetApp/views.py
+++ ManageSnippetApp/SnippetApp/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
  # SnippetApp/views.py
 from django.views.generic import TemplateView
+from django.shortcuts import Http404
 
 from .models import Snippet
 from .forms import CreateSnippet
 # Create your views here.
-
-
-# Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         form = CreateSnippet()
-        return render(request, 'index.html', {'form': form},{'Snippet':Snippet})
-
-def post_new(request):
-    if request.method == "POST":
+        #try:
+        snippets = Snippet.objects.all()
+        #except snippet.DoesNotExist:
+        #raise Http404("Snippets does not exist")
+        return render(request, 'index.html', {'form': form, "snippet": snippets})
+    def post(self, request, **kwargs):
         form = CreateSnippet(request.POST)
+        # verify that form is valid
         if form.is_valid():
-            Snippet = form.save(commit=False)
-            Snippet.title = request.title
-            Snippet.language = request.language
-            Snippet.code_snippet = request.code_snippet
-            Snippet.description = request.description
-            Snippet.save()
-            #return redirect('post_detail', pk=post.pk)
-    else:
-        form = CreateSnippet()
-    return render(request, 'index.html', {'form': form}, {'Snippet':Snippet})        
+        # save the form data to database
+            form.save(commit=True)
+            snippets = Snippet.objects.all() 
+                #snippet.DoesNotExist:
+                #raise Http404("Snippets does not exist")
+            form = CreateSnippet()
+            #print("Snipettttttttttt Language/.............",snippet.title)
+        return render(request, 'index.html', {'form': form, "snippet": snippets})
+        #################################################################################
+          
